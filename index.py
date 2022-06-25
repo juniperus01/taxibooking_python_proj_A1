064be056fee6938fb486b6dfa2ce68f80c4593e3
Jiya Bhagat <97676121+juniperus01@users.noreply.github.com>
2022-06-25 21:19:59 +0530

    Create index.py
    Started with the login page

---
+++ index.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+
+if __name__ == '__main__':
+    root = Tk()
+
+    # =========================================== Getting Screen Width ==================================================================
+    w = root.winfo_screenwidth()
+    h = root.winfo_screenheight()
+    geometry = "%dx%d+%d+%d" % (w, h, 0, 0)
+
+    root.geometry("500x300+320+200")
+    root.title('Login Form')
+  
+    root.mainloop()
