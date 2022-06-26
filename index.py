2b9cd6e0074d69bf36a441574ce758dd0429cd34
Jiya Bhagat <97676121+juniperus01@users.noreply.github.com>
2022-06-26 12:22:08 +0530

    Update index.py
    updated user class and defined widgets

--- index.py
+++ index.py
@@ -11,7 +11,21 @@ class user:
         self.password = StringVar()
         self.n_username = StringVar()
         self.n_password = StringVar()
+        # Create Widgets
+        self.widgets()
         
+      # Draw Widgets
+    def widgets(self):
+        self.head = Label(self.master, text='LOGIN', font=('', 35), pady=10)
+        self.head.pack()
+        self.logf = Frame(self.master, padx=10, pady=10)
+        Label(self.logf, text='Username: ', font=('', 20), pady=5, padx=5).grid(sticky=W)
+        Entry(self.logf, textvariable=self.username, bd=5, font=('', 15)).grid(row=0, column=1)
+        Label(self.logf, text='Password: ', font=('', 20), pady=5, padx=5).grid(sticky=W)
+        Entry(self.logf, textvariable=self.password, bd=5, font=('', 15), show='*').grid(row=1, column=1)
+        Button(self.logf, text=' Login ', bd=3, font=('', 15), padx=5, pady=5, command=self.login).grid()
+        Button(self.logf, text=' Create Account ', bd=3, font=('', 15), padx=5, pady=5, command=self.cr).grid(row=2,
+               
 
 if __name__ == '__main__':
     root = Tk()
